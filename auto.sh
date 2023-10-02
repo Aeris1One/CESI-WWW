b773b24b775b82a10016c6fffd90d5561ee5b09d
Charles P <charles.pingard@viacesi.fr>
2023-10-02 10:53:15 +0200

    ✨ feat: corbeille étape 2

--- auto.sh
+++ auto.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-file='.'
 while getopts "f:" option; do
    case $option in
       f)
-        file=$OPTARG;;
+        root_dir=$OPTARG
+        build_dir="$root_dir/build"
+        tmp_dir="$root_dir/.tmp";;
       \?)
         echo "Invalid option"
         exit;;
    esac
 done
 
-ls "$file"
+# Exit if no directory provided
+if [ -z "$root_dir" ]; then
+  echo "No directory provided, see -f option"
+fi
+
+# Create or empty build
+if [ ! -d "$build_dir" ]; then
+  mkdir "$build_dir"
+else
+  # Good practice: le ${variable:?} fait crash le script si la variable
+  # n'est pas définie, évite de supprimer tous les fichiers au cas où.
+  rm -rfv "${build_dir:?}/"*
+fi
+
+# Create or emput .tmp
+if [ ! -d "$tmp_dir" ]; then
+  mkdir "$tmp_dir"
+else
+  rm -rfv "${tmp_dir:?}/"*
+fi
