dd19859e81560a84a1b45b9624017e613ceb29c6
Charles P <charles.pingard@viacesi.fr>
2023-10-02 11:58:59 +0200

    ✨ feat: corbeille étape 4

--- auto.sh
+++ auto.sh
@@ -14,28 +14,45 @@ done
 
 # Exit if no directory provided
 if [ -z "$root_dir" ]; then
-  echo "No directory provided, see -f option"
+  echo 'No directory provided, see -f option'
 fi
+
+echo 'Étape 1 : Création des dossiers nécessaires'
 
 # Create or empty build
 if [ ! -d "$build_dir" ]; then
+  echo '- Création du dossier build/'
   mkdir "$build_dir"
 else
   # Good practice: le ${variable:?} fait crash le script si la variable
   # n'est pas définie, évite de supprimer tous les fichiers au cas où.
+  echo '- Vidange du dossier build/'
   rm -rfv "${build_dir:?}/"*
 fi
 
 # Create or emput .tmp
 if [ ! -d "$tmp_dir" ]; then
+  echo '- Création du dossier .tmp/'
   mkdir "$tmp_dir"
 else
+  echo '- Vidange du dossier .tmp/'
   rm -rfv "${tmp_dir:?}/"*
 fi
 
+echo 'Étape 2 : Compilation des fichiers objets'
 for c in "$root_dir/"*.c; do
-  avr-gcc -c \
+  c=$(basename "$c")
+  o="${c%.*}.o"
+  avr-gcc \
       -std=gnu17 -Os -Wall -DF_CPU=16000000 -mmcu=atmega328p \
-      "$c" \
-      -o "$tmp_dir/${c%.*}.o"
+      -c "$c" \
+      -o "$tmp_dir/$o"
+  o_file_list="$o_file_list $tmp_dir/$o"
 done
+
+echo 'Étape 3 : Création des liens'
+  # shellcheck disable=SC2086
+avr-gcc \
+      -std=gnu17 -Os -Wall -DF_CPU=16000000 -mmcu=atmega328p \
+      $o_file_list \
+      -o "$build_dir/output.elf"
