fe16ec24597874a517193c2bc67a837d5888cea6
Charles P <charles.pingard@viacesi.fr>
2023-10-25 10:12:10 +0200

    :recycle: refactor: use timer interrupt to ensure output and input update

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@
 #include <lib/input/input.h>
 #include "lib/output/led.h"
 
-#define DEBUG 1
+// Setup Timer Interrupts
+#define USE_TIMER_1 true
+#define USE_TIMER_2 true
+
+#include <TimerInterrupt.h>
+
+
+#define DEBUG 0
 
 #define RED_PIN 4
 #define GREEN_PIN 3
@@ -10,37 +17,36 @@
 #define LED_DIN 6
 
 #if DEBUG
-
 #include <avr8-stub.h>
-
 #endif
 
 Input input(RED_PIN, GREEN_PIN);
 Output output(LED_CIN, LED_DIN);
 
+#include <lib/globals.h>
+
+void timer1_callback() {
+    output.update();
+}
 
 void setup() {
 #if DEBUG
     debug_init();
-#endif
     delay(1000);
-    output.set_mode(0);
-    while (millis() < 5000) {
-        input.update();
-        if (input.is_red_pressed()) {
-            output.set_mode(2);
-            return;
-        }
-    }
-    output.set_mode(1);
+#endif
+
+    output.set_mode(5);
+    output.update();
+
+    ITimer1.init();
+    ITimer1.attachInterruptInterval(100, timer1_callback);
+
+    Serial.begin(9600);
 }
 
 
 void loop() {
-    output.update();
-    input.update();
-    if (input.was_red_pressed_for(5000)) {
-        output.set_mode(4);
-        return;
-    }
+    Serial.println("Hello, world!");
+    while (1);
+    Serial.println("Goodbye, world!");
 }
