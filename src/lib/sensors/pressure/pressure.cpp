5030559881848a527ad1cb13a2b96753af0b7731
theo <theo.dalletthuillier@viacesi.fr>
2023-10-25 16:26:34 +0200

    add bme library

--- src/lib/sensors/pressure/pressure.cpp
+++ src/lib/sensors/pressure/pressure.cpp
@@ -1,16 +1,17 @@
 #include <Arduino.h>                                            // Initialisation des différentes bibliothèques 
 #include <Wire.h>
 #include <math.h>
+#include <BME280I2C.h> 
+
+BME280 bme;
+bme.begin();
 
 float get_pressure(){                                          
     
     const int broche_capteur = A0;                              // Définition des broches de connexion du capteur
-    int valeur_capteur = analogRead(broche_capteur);            // Lecture de la valeur du capteur
+    int valeur_capteur = bme.readPressure() ;            // Lecture de la valeur du capteur
     float pression = valeur_capteur * 25 / 1023;                // Conversion de la valeur du capteur en pression
 
-    //Serial.println(pression);                                 // Affichage de la valeur de la pression
-
     delay(1000);
-    Serial.print(pression);
     return pression;
 }
