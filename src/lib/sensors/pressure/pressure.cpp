ca7f58fe5c899c3339fde4f73926f57f33063649
theo <theo.dalletthuillier@viacesi.fr>
2023-10-25 15:43:25 +0200

    pressure function

--- src/lib/sensors/pressure/pressure.cpp
+++ src/lib/sensors/pressure/pressure.cpp
@@ -1,4 +1,15 @@
-float get_pressure(){
+#include <Arduino.h>
+#include <Wire.h>
+#include <math.h>
 
+float get_pressure(){
     
+    const int brocheCapteur = A0;                               // Définition des broches de connexion du capteur
+    int valeurCapteur = analogRead(brocheCapteur);              // Lecture de la valeur du capteur
+    float pression = valeurCapteur * 25 / 1023;                 // Conversion de la valeur du capteur en pression
+
+    //Serial.println(pression);                                   // Affichage de la valeur de la pression
+
+    delay(1000);
+    return pression;
 }
