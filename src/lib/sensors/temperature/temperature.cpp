cef62392fefceb9a5a2bc19d31bed4cf7f87377c
Maxence <maxence.david@viacesi.fr>
2023-10-26 09:52:42 +0200

    temperature completed

--- src/lib/sensors/temperature/temperature.cpp
+++ src/lib/sensors/temperature/temperature.cpp
@@ -1,12 +1,31 @@
 #include <Arduino.h>    //Bibliothèque permettant d'utiliser les instructions en Arduino
-#include <Wire.h>       //Bibliothèque permettant de se connecter au capteur
+#include <BME280I2C.h>
+#include <Wire.h>
+
+void setup(){
+    
+    Wire.begin();
+    BME280I2C bme;
+
+      while(!bme.begin())
+        {
+            Serial.println("Nous n'avons pas trouvé le capteur");
+            delay(1000);
+        }
+
+}  
+
 
 float get_temperature(){ // On donne le nom de notre fonction pour récupérer les valeurs de température en flottant
     
-        int broche= A5;// On spécifie sur quelle broche on décide de placer notre capteur
-        int lecture_val_capteur=analogRead(broche);
-        float temperature=lecture_val_capteur ;
-
+    BME280I2C bme;
+    bme.begin();
+        
+        float temperature;
+        temperature=bme.temp();
+        
+        BME280I2C::TempUnit tempUnit(BME280I2C::TempUnit_Celsius);  
+         
         delay(1000);
         Serial.print("La température est de :");
         Serial.print(temperature);
@@ -14,5 +33,3 @@ float get_temperature(){ // On donne le nom de notre fonction pour récupérer l
     
     return(temperature);
 }
-
-//Pas sur que ça fonctionne car j'ai cru voir qu'il fallait utiliser une bibliothèque type adafruit.
