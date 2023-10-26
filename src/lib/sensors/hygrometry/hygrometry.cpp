0a9cef2c38233fd0b5320c0c7fe43a8f59303538
Jeremy Maille <jeremy.maille@viacesi.fr>
2023-10-26 10:54:07 +0200

    create get_hygrometry

--- src/lib/sensors/hygrometry/hygrometry.cpp
+++ src/lib/sensors/hygrometry/hygrometry.cpp
@@ -1,9 +1,23 @@
-#include <BME280I2C.h>
+#include <Arduino.h>
 #include <Wire.h>
+#include <BME280I2C.h>
 
-float get_hygrometry(){
-  BME280I2C bme;
-  bme.begin();
-  float hygrometry = bme.hum();
-  return hygrometry;
+
+float get_hygrometry() {
+    unsigned long startTime = millis();
+    while (millis() - startTime < TIMEOUT) {
+        BME280I2C bme;
+
+        Wire.begin();
+
+        bme.begin();
+
+        float hygrometry = bme.hum();
+        if (hygrometry < HYGR_MINT || hygrometry > HYGR_MAXT) {
+            return -1; // données incohérentes
+        } else {
+            return hygrometry;
+        }
+    }
+    return -2; // timeout
 }
