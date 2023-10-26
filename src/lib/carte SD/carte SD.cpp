c41192f9418b3808bcf3664fd40e65a9bce7baf9
theo <theo.dalletthuillier@viacesi.fr>
2023-10-26 11:42:24 +0200

    Finish Save_SD function

--- src/lib/carte SD/carte SD.cpp
+++ src/lib/carte SD/carte SD.cpp
@@ -14,5 +14,33 @@ void Save_SD(int lum, int temp, int hygr, int pres, int time){
             return ;
         }
     }
-    
+    int taille = fichier.size(),
+    if (taille > MAX_FILE_SIZE){
+        int x = 1 ;
+        while (true){
+            File fichierExistant = SD.open("YYMMDD" + string(x) + ".log");
+            if (!fichierExistant){
+                fichier.rename("YYMMDD" + string(x) + ".log");
+                break;
+            }
+            x++;
+        }
+        fichier = SD.open("YYMMDD_0.log", FILE_WRITE);
+         if (!fichier) {
+            Serial.println("Erreur de création de fichier");
+            return;
+            }
+    }
+    fichier.print(lum);
+    fichier.print(";");
+    fichier.print(temp);
+    fichier.print(";");
+    fichier.print(hygr);
+    fichier.print(";");
+    fichier.print(press);
+    fichier.print(";");
+    fichier.print(time);
+    fichier.println();
+
+    fichier.close();
 }
