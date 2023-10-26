23698a4e82c3eafaefc3421341c2461cec192003
theo <theo.dalletthuillier@viacesi.fr>
2023-10-26 11:17:43 +0200

    Initialization of SD

--- src/lib/carte SD/carte SD.cpp
+++ src/lib/carte SD/carte SD.cpp
@@ -2,6 +2,10 @@
 #include <SD.h>
 
 void Save_SD(int lum, int temp, int hygr, int pres, int time){
-
+    if (!SD.begin()){
+        serial.println("Erreur lors du lancement de la carte SD");
+        return;
+    }
+    
 
 }
