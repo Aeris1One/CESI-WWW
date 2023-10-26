b66aa8c3f7874d08e60acf321b6f787ff286266b
theo <theo.dalletthuillier@viacesi.fr>
2023-10-26 10:07:41 +0200

    add comments to pressure.cpp

--- src/lib/sensors/pressure/pressure.cpp
+++ src/lib/sensors/pressure/pressure.cpp
@@ -1,23 +1,22 @@
-#include <Arduino.h>
+#include <Arduino.h>                                    //Ajout des librairies nécessaire 
 #include <Wire.h>
 #include <BME280I2C.h>
 
 
 float get_pressure() {
     unsigned long startTime = millis();
-    while (millis() - startTime < 5000) {
-        BME280I2C bme;
+    while (millis() - startTime < 5000) {               // On test depuis combien de temps la fonction est lancé
+        BME280I2C bme;                              
 
         Wire.begin();
 
-        bme.begin();
+        bme.begin();                                    // Lancement du capteur
 
-        float pressure = bme.pres();
-        if (pressure < 850 or pressure > 1080) {
-            return -1; // données incohérentes
+        float pressure = bme.pres();                    // Assigne la valeur du capteur à la variable
+        if (pressure < 850 or pressure > 1080) {        // On test si les données sont cohérentes ou non 
+            return -1;                                  // Données incohérentes
         } else {
-            return pressure;
+            return pressure;                            // Données cohérentes donc on retourne la valeur                                                      
         }
     }
-    return -2; // timeout
-}
+    return -2;                                          // Timeout
