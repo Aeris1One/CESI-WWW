52ee8b1f7c5f903d689e71307cd24a4ddc53997b
theo <theo.dalletthuillier@viacesi.fr>
2023-10-26 11:31:31 +0200

    Verification if fichier exist

--- src/lib/carte SD/carte SD.cpp
+++ src/lib/carte SD/carte SD.cpp
@@ -6,6 +6,13 @@ void Save_SD(int lum, int temp, int hygr, int pres, int time){
         serial.println("Erreur lors du lancement de la carte SD");
         return;
     }
+    File fichier = SD.open("YYMMDD_0.log");
+    if (!fichier){
+        fichier = SD.open("YYMMDD_0.log",FILE_WRITE);
+        if (!fichier){
+            Serial.println("Erreur de création du fichier");
+            return ;
+        }
+    }
     
-
 }
