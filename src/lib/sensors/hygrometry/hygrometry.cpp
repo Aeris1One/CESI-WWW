1f77792fc0dfe460c24c20cab4ef7ddd86dd96c0
Jeremy Maille <jeremy.maille@viacesi.fr>
2023-10-26 09:48:36 +0200

    update hygrometry

--- src/lib/sensors/hygrometry/hygrometry.cpp
+++ src/lib/sensors/hygrometry/hygrometry.cpp
@@ -1,8 +1,9 @@
 #include <BME280I2C.h>
+#include <Wire.h>
 
 float get_hygrometry(){
-    BME280 bme;
-    bme.begin()
-    float hygrometry = bme.readHumidity();
-    return hygrometry;
+  BME280I2C bme;
+  bme.begin();
+  float hygrometry = bme.hum();
+  return hygrometry;
 }
