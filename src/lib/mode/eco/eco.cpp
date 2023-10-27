79f00d6676e39a7db48dc712d40a7c7ce22300b5
Maxence <maxence.david@viacesi.fr>
2023-10-27 11:32:47 +0200

    bidouillage

--- src/lib/mode/eco/eco.cpp
+++ src/lib/mode/eco/eco.cpp
@@ -8,6 +8,8 @@
 #include <lib/sensors/RTC/horloge.h>
 #include <lib/sensors/GPS/gps.h>
 
+bool gps_getvalue = true;
+
 void eco_setup(){
     mode=2;
 
@@ -16,7 +18,19 @@ void eco_setup(){
 void eco_loop(){
 
     if(timer >= get_LOG_INTERVAL()*20){
-        Save_SD(get_horloge(), get_luminosity(), get_temperature(), get_hygrometry(), get_pressure(), get_gps());
+        if(gps_getvalue){
+            Save_SD(get_horloge(), get_luminosity(), get_temperature(), get_hygrometry(), get_pressure(), get_gps());
+        }
+
+        else{
+            Save_SD(get_horloge(), get_luminosity(), get_temperature(), get_hygrometry(), get_pressure(), "");
+        }
+        gps_getvalue = !gps_getvalue;
+
         timer = 0;
     }
+
+
+
+        
 }
