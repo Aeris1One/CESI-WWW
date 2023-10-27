fafd533c79a4f44df4a36b818f0e7b9f3f6c06d5
Jeremy Maille <jeremy.maille@viacesi.fr>
2023-10-27 14:12:23 +0200

    comment serial in carte sd

--- src/lib/carte SD/carte SD.cpp
+++ src/lib/carte SD/carte SD.cpp
@@ -22,7 +22,7 @@ void Save_SD(DateTime time, int lum, int temp, int hygr, int pres, String gps_da
     file = sd.open(filename_base + String("_0.log"), FILE_WRITE);
 
     if (file.size() > 2048) {
-        Serial.println("File too big, renaming");
+        //Serial.println("File too big, renaming");
         int x = 1;
         String filename_loop = filename_base + String("_") + String(x) + String(".log");
         while (sd.exists(filename_loop)) {
@@ -30,8 +30,8 @@ void Save_SD(DateTime time, int lum, int temp, int hygr, int pres, String gps_da
             filename_loop = filename_base + String("_") + String(x) + String(".log");
             Serial.println(filename_loop);
         }
-        Serial.print("New filename: ");
-        Serial.println(filename_loop);
+        //Serial.print("New filename: ");
+        //Serial.println(filename_loop);
         char filename_buf[filename_loop.length() + 1];
         filename_loop.toCharArray(filename_buf, filename_loop.length() + 1);
         file.rename(filename_buf);
@@ -43,7 +43,7 @@ void Save_SD(DateTime time, int lum, int temp, int hygr, int pres, String gps_da
         Serial.println("Error opening file");
         return;
     }
-    Serial.println("Writing to file");
+    //Serial.println("Writing to file");
 
     String line = String(time.timestamp()) + String(";")
                   + String(lum) + String(";")
@@ -52,8 +52,8 @@ void Save_SD(DateTime time, int lum, int temp, int hygr, int pres, String gps_da
                   + String(pres) + String(";")
                   + gps_data;
     file.println(line);
-    Serial.print("Wrote: ");
-    Serial.println(line);
+    //Serial.print("Wrote: ");
+    //Serial.println(line);
     file.close();
-    Serial.println("Done");
+    //Serial.println("Done");
 }
